0b8c997e245766c462b51c9835f260e8890ffc2c
Simon Flutura <simon.flutura@informatik.uni-augsburg.de>
2017-01-12 14:55:29 +0100

    openface linux

---
+++ plugins/openface/include/OpenfaceHelper.h
@@ -0,0 +1,68 @@
+#pragma once
+
+
+// Libraries for landmark detection (includes CLNF and CLM modules)
+#include "LandmarkCoreIncludes.h"
+#include "GazeEstimation.h"
+#include "FaceAnalyser.h"
+
+#include <fstream>
+#include <sstream>
+
+// OpenCV includes
+#include <ssiocv.h>
+
+// Boost includes
+#if __gnu_linux__
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#else
+#include <filesystem.hpp>
+#include <filesystem/fstream.hpp>
+#endif
+namespace ssi {
+
+	class OpenfaceHelper
+	{
+
+	public:
+
+		OpenfaceHelper(std::vector<std::string> arguments, std::string action_units_location, std::string triangulation_location);
+		OpenfaceHelper();
+		~OpenfaceHelper();
+
+		/*			/////For Tracking/////			*/
+                bool update_landmark(cv::Mat &captured_image);
+                void update_faceanalyser(cv::Mat &captured_image, bool video_input, double fps);
+                cv::Vec6d get_pose_camera();
+                cv::Vec6d get_pose_world();
+                cv::Vec6d get_corrected_pose_camera();
+                cv::Vec6d get_corrected_pose_world();
+                std::vector<cv::Point2d> get_detected_landmarks();
+                double get_detection_certainty();
+                std::vector<cv::Point3f> get_gaze_directions();
+                std::vector<float> calculate_coi(cv::Mat captured_image);
+                std::vector<float> calculate_fl(cv::Mat captured_image);
+                std::vector<cv::Point3f> get_pupil_position();
+                std::vector<double> get_current_action_unit_reg();
+                std::vector<double> get_current_action_unit_classes();
+
+		/*			/////For Drawing/////			*/
+                void draw_landmarks(cv::Mat& captured_image, vector<cv::Point> landmarks);
+                void draw_box(cv::Mat captured_image, cv::Vec6d pose, cv::Scalar color, int thickness, float fx, float fy, float cx, float cy);
+                void draw_gaze(cv::Mat img, cv::Point3f gazeVecAxisLeft, cv::Point3f gazeVecAxisRight, cv::Point3f pupil_left, cv::Point3f pupil_right, float fx, float fy, float cx, float cy);
+                vector<pair<cv::Point, cv::Point>> calculate_box(cv::Vec6d pose, float fx, float fy, float cx, float cy);
+		// Some globals for tracking timing information for visualisation
+		int frame_count;
+		float fx, fy, cx, cy;
+
+		LandmarkDetector::FaceModelParameters *det_parameters;
+		// The modules that are being used for tracking
+		LandmarkDetector::CLNF *clnf_model;
+
+		// Creating a  face analyser that will be used for AU extraction
+		FaceAnalysis::FaceAnalyser *face_analyser;
+
+	};
+
+}
